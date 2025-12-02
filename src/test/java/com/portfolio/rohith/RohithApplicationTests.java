59b02b7448b1204ec82c87abf9ebd78cb0497633
Rohith Nair <rohithnair2910@gmail.com>
2025-12-02 20:51:15 +0530

    initial commit

---
+++ src/test/java/com/portfolio/rohith/RohithApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.rohith;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RohithApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
