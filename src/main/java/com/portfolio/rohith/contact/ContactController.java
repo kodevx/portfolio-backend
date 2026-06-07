bdd17e6a4eacda30b3ebb3ac0377adc4a6c62081
Rohith Nair <rohithnair2910@gmail.com>
2026-06-07 11:35:12 +0530

    fix: cross-origin url fix for production website

--- src/main/java/com/portfolio/rohith/contact/ContactController.java
+++ src/main/java/com/portfolio/rohith/contact/ContactController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/contact")
-@CrossOrigin(origins = { "http://localhost:8000" })
+@CrossOrigin(origins = { "https://rnair.tech/", /*"http://localhost:8000"*/ })
 public class ContactController {
 
     private final ContactService contactService;
--- src/main/java/com/portfolio/rohith/snaps/SnapsController.java
+++ src/main/java/com/portfolio/rohith/snaps/SnapsController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/snaps")
-@CrossOrigin(origins = { "http://localhost:8000", "http://192.168.1.35:8000/" })
+@CrossOrigin(origins = { "https://rnair.tech/", /*"http://localhost:8000"*/ })
 public class SnapsController {
 
     public final SnapsService snapsService;
